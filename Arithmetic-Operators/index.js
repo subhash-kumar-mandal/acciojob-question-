39c8a141bc4ef03e716a39c107110050ddd20925
subhash kumar mandal <pm817405@gmail.com>
2025-09-29 07:01:51 +0530

    find fahreneit

--- Arithmetic-Operators/index.js
+++ Arithmetic-Operators/index.js
@@ -20,9 +20,28 @@
 
 
 let area_peremeter = (radius) => {
-    const  pi = 3 ;
-    console.log(`Area of circle  -> ${pi*radius*radius}`)
-    console.log(`Perimeter of circle  -> ${2*pi*radius}`)
+    const pi = 3;
+    console.log(`Area of circle  -> ${pi * radius * radius}`)
+    console.log(`Perimeter of circle  -> ${2 * pi * radius}`)
 }
 
-area_peremeter(4)
+// area_peremeter(4)
+
+
+//  Question 2. Celsius to Fahrenheit,
+//     Given a temperature in Celsius, output the fahrenheit equivalent of it.
+//     This is similar to the Celsius to Fahrenheit 1 question, read the note below carefully.
+
+//     note ---> Print complete answer up to 6 decimal places.
+//     EXAMPLE 1
+//     Input:
+//     32
+//     Output::
+//     89.600000
+
+function fahreneit_find(c){
+    let f =(c*(9/5))+32 // find fahreneit 
+    return f.toFixed(6)  //fixed last 6 decimal places
+}
+
+console.log(fahreneit_find(32))
