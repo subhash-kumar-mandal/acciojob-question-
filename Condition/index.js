a13d4233e6bed040d5b1f06e8318258c901f9f80
subhash kumar mandal <pm817405@gmail.com>
2025-09-29 22:32:05 +0530

    queston 1

---
+++ Condition/index.js
@@ -0,0 +1,14 @@
+// 1. Number Comparison
+// If the number is greater than 1, then return You entered more
+// If the number is less than or equal to 1 then return You entered less
+// Example 1
+// Input
+// 1
+// Output
+// You entered less
+let n=7;
+ if(n<=1){
+    console.log("You entered less")
+  }else{
+    console.log("You entered more")
+  }
