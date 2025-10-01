4f1dd906ca96e97888af63237c415e21730d9e2a
subhash kumar mandal <pm817405@gmail.com>
2025-10-01 12:08:18 +0530

    question 2

--- Condition-1/index.js
+++ Condition-1/index.js
@@ -5,7 +5,7 @@
 // for marks above 60 and less than equal to 70, print "Meets Expectations".
 // for marks below 60, print "Below Expectations".
  let n= 95 ;
- 
+
   if(n>90 && n<=100){
      console.log("Excellent")
   }else if(n>80 && n<=90){
@@ -17,4 +17,29 @@
   }else{
     console.log("Below Expectations")
   }
+
+// 2. Leap Year  check . 
+// A Leap year is the year that is multiple of 4. However, multiples of 100 except for the multiples of 400 are
+//  not leap years. Your task is to complete the function isLeapYear which receives the year as its parameter 
+//  and returns 1 or 0 depending upon if the year is a leap year or not.
+
+
+const isLeapYear = (year) => {
+    if(year % 4 === 0){
+      if(year % 100 === 0){
+        if(year % 400 === 0){
+          return 1;   // divisible by 400 → leap
+        } else {
+          return 0;   // divisible by 100, not 400 → not leap
+        }
+      } else {
+        return 1;     // divisible by 4, not by 100 → leap
+      }
+    } else {
+      return 0;       // not divisible by 4 → not leap
+    }
+  }
+
+  console.log(isLeapYear(1900))
+
   
